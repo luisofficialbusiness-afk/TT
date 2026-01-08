41533b2408d315ded6fa5b77b6215808847a48cc
RVEPRTY <luisofficialbusiness@gmail.com>
2026-01-08 17:44:18 -0600

    Update typing.js

--- api/typing.js
+++ api/typing.js
@@ -1,19 +1,15 @@
 let typingUsers = [];
 
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    const { user, typing } = req.body;
-    if (typing) {
-      if (!typingUsers.includes(user)) typingUsers.push(user);
+  if(req.method === "POST") {
+    const {user, typing} = req.body;
+    if(typing) {
+      if(!typingUsers.includes(user)) typingUsers.push(user);
     } else {
-      typingUsers = typingUsers.filter(u => u !== user);
+      typingUsers = typingUsers.filter(u=>u!==user);
     }
-    return res.status(200).json({ ok: true });
+    return res.status(200).json({ok:true});
   }
-
-  if (req.method === "GET") {
-    return res.status(200).json({ users: typingUsers });
-  }
-
+  if(req.method === "GET") return res.status(200).json({users:typingUsers});
   res.status(405).end();
 }
