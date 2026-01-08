a6cdf9604a1a0ec0c4a9c1c4c1c4d3cbb376332f
RVEPRTY <luisofficialbusiness@gmail.com>
2026-01-08 17:15:12 -0600

    Update and rename pages/api/messages.js to api/messages.js

--- api/messages.js
+++ api/messages.js
@@ -2,13 +2,8 @@ let messages = [];
 
 export default function handler(req, res) {
   if (req.method === "POST") {
-    messages.push({
-      user: req.body.user,
-      text: req.body.text
-    });
-
+    messages.push({ user: req.body.user, text: req.body.text });
     if (messages.length > 100) messages.shift();
-
     return res.status(200).json({ ok: true });
   }
 
